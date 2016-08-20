7713c391e6fa59f13679c70ded43282bf6b40184
rdamico0 <roberto.damico.1@studenti.unipd.it>
2016-08-20 22:44:35 +0200

    quick fix 11

--- server/config.local.js
+++ server/config.local.js
@@ -1,7 +1,7 @@
 if (typeof process.env.PORT !== 'undefined' || typeof process.env.HOST !== 'undefined')
 
     module.exports = { port: process.env.PORT,
-					   host: process.env.HOST
+					   host: 'mass-demo.herokuapp.com'
 };
 else
     module.exports = { };
