808fe207b52a475ba2f0efb38092d8f027a13915
rdamico0 <roberto.damico.1@studenti.unipd.it>
2016-08-29 16:56:34 +0200

    nope reverted

--- server/boot/root.js
+++ server/boot/root.js
@@ -3,13 +3,5 @@ module.exports = function(server) {
   var router = server.loopback.Router();
   router.get('/status', server.loopback.status());
 
-  router.get('*.*', function (request, response){
-    response.sendFile(path.resolve('client' + request.url))
-  });
-
-  router.get('*', function (request, response){
-    response.sendFile(path.resolve('client', 'index.html'))
-  })
-  
   server.use(router);
 };
