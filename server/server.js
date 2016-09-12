eb53257433f006da82b20a8665731e2f0c6180ea
rdamico0 <roberto.damico.1@studenti.unipd.it>
2016-09-12 05:41:54 +0200

    env variable try 2

--- server/server.js
+++ server/server.js
@@ -6,8 +6,7 @@ var app = module.exports = loopback();
 app.start = function() {
   // start the web server
   app.set('jwtTokenSecret', process.env.JWT);
-  console.log(process.env.JWT);
-  app.set('clienthost', 'maas-demo.herokuapp.com');
+  app.set('clienthost', process.env.ROOTURL);
   return app.listen(function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
